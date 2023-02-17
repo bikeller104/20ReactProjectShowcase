c391b054b4769cffd26a341083106088e8fac003
Brent Keller <bikeller104@gmail.com>
2023-02-17 14:35:02 -0700

    updated file name to display page

--- react-app/src/components/ContactMe.js
+++ react-app/src/components/ContactMe.js
@@ -3,9 +3,7 @@ import React from 'react';
 
 function ContactMe() {
     return (
-        <>
-        <p>contactMe</p>
-        </>
+        <p style={{ height: '1vh'}}>contactMe</p>
         )
     }
     
