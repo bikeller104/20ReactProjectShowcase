c0943afb62bcac0956cee62cab05f813895cd975
Brent Keller <bikeller104@gmail.com>
2023-02-20 08:33:52 -0700

    started contact me form

--- react-app/src/components/ContactMe.js
+++ react-app/src/components/ContactMe.js
@@ -2,10 +2,22 @@ import React from 'react';
 import '../Styles/MainComponent.css';
 
 
+function lostFocus() {
+    //document.getElementById("email_input").text")
+}
+
 function ContactMe() {
     return (
         <main className='main_container'>
-            <p>contactMe</p>
+            <section className='content_bounds'>
+                <h3>Contact Me</h3>
+                <p>Name:</p>
+                <input type="text" ></input>
+                <p>Email Address:</p>
+                <input type="email" id="email_input"></input>
+                <p>Message</p>
+                <textarea rows="10" cols="40"></textarea>
+            </section>
         </main>
         )
     }
