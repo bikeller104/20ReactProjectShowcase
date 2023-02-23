063ce02148e3ccba0f65f3e29214444b6addb1e8
Brent Keller <bikeller104@gmail.com>
2023-02-22 19:47:46 -0700

    created resume page

--- react-app/src/components/Resume.js
+++ react-app/src/components/Resume.js
@@ -5,7 +5,33 @@ import '../Styles/MainComponent.css';
 function Resume() {
     return (
         <main className='main_container'>
-            <p>Resume</p>
+            <section>
+
+            <h2>Skills</h2>
+            <ul>
+                <li>React</li>
+                <li>Webpack</li>
+                <li>css</li>
+                <li>JavaScript</li>
+                <li>Java</li>
+            </ul>
+            </section>
+            <section>
+
+<h2>Front-End Projects</h2>
+<ul>
+    <li>Web Based Notes App</li>
+    <li>Profile Website</li>
+    <li>Weather Forecast Dashboard App</li>
+</ul>
+
+<h2>Back-End Projects</h2>
+<ul>
+    <li>E-Commerse Backend</li>
+    <li>Employee Database</li>
+
+</ul>
+</section>
         </main>
         )
     }
