11ee69476e08693f24960aa2a656863a7b4d37aa
Brent Keller <bikeller104@gmail.com>
2023-02-25 11:27:16 -0700

    added projects to portfolio

--- react-app/src/components/Portfolio.js
+++ react-app/src/components/Portfolio.js
@@ -9,12 +9,26 @@ const weatherProject = {
     projectLink: `https://github.com/bikeller104/06CustomizableWeatherLookup`
 }
 
+const LiveNoteTaker = {
+    projectLink: `https://github.com/bikeller104/11LiveNoteTaker`,
+    projectTitle: `Live Note Taker`,
+    imgSource: `https://raw.githubusercontent.com/bikeller104/11LiveNoteTaker/main/ProjectVisual.png`
+}
+
+const TimedQuiz = {
+    imgSource: `https://raw.githubusercontent.com/bikeller104/04TimedJavascriptQuiz/main/Final%20Screenshot.png`,
+    projectTitle: `Timed Quiz`,
+    projectLink: `https://github.com/bikeller104/04TimedJavascriptQuiz`
+}
+
 function Portfolio() {
     return (
         <main className='main_container'>
             <ProjectPane imgSource= {weatherProject.imgSource} projectTitle={weatherProject.projectTitle} projectLink={weatherProject.projectLink}>
 
             </ProjectPane>
+            <ProjectPane {...LiveNoteTaker}/>
+            <ProjectPane {...TimedQuiz}/>
         </main>
         )
     }
